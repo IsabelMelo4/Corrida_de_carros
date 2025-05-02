b679c2b76b5923972251715f28ef8bc919ba7110
IsabelMelo4 <isabelmelo630@gmail.com>
2025-05-02 11:00:35 -0300

    primeiro commit

---
+++ jogo_de_corrida.py
@@ -0,0 +1,40 @@
+import random
+import time
+
+jogador1 = input("Jogador 1: ")
+jogador2 = input("Jogador 2: ")
+
+print("O jogo vai começar em:") 
+
+for contador in range(3, 0, -1):
+       time.sleep(1.2)
+       print(contador) 
+
+pos1 = 0
+pos2 = 0
+
+linha_de_chegada = 30
+
+
+carro1 = """       
+       ____ 
+   __/      \__ 
+  [  @ ____ @  ]""" 
+
+carro2 = """       
+       ____ 
+   __/      \__ 
+  [  @ ____ @  ] """ 
+
+while pos1 < linha_de_chegada and pos2 < linha_de_chegada:
+        time.sleep(0.5)
+
+        #bibliotecas
+    
+        mov1 = random.randint(1, 5)
+        mov2 = random.randint(1, 5)
+    
+
+        pos1 += mov1
+        pos2 += mov2 
+
